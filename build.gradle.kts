e0a08a95c277088bfaac0d36f69eb121dcd26392
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-05-02 13:50:48 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
     id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
+    // ADD THIS LINE BELOW
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
